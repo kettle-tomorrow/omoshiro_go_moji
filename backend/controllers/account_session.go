4dd6959697e9e5607de03a143f085d32867d3caa
tomoro kobori <46224993+TomoroKobori@user.noreply.github.com>
2022-07-10 19:24:55 +0900

    refactor: controller/loginの分岐を修正

--- backend/controllers/account_session.go
+++ backend/controllers/account_session.go
@@ -12,26 +12,28 @@ import (
 
 func Login(c *gin.Context) {
 	requestAccount := models.Account{}
-	err := c.Bind(&requestAccount)
-	if err != nil {
+	bindErr := c.Bind(&requestAccount)
+	if bindErr != nil {
 		c.String(http.StatusBadRequest, "login failed1")
-	} else {
-		account := models.Account{}
-		account = account.GetAccountByEmail(requestAccount.Email)
-
-		err = bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(requestAccount.Password))
-		if err != nil {
-			c.String(http.StatusBadRequest, "login failed2")
-		} else {
-			session := sessions.Default(c)
-			loginUser, err := json.Marshal(account)
-			if err == nil {
-				session.Set("loginUser", string(loginUser))
-				session.Save()
-				c.Status(http.StatusOK)
-			} else {
-				c.Status(http.StatusInternalServerError)
-			}
-		}
+		return
 	}
+
+	account := models.Account{}
+	account = account.GetAccountByEmail(requestAccount.Email)
+	comparePasswordErr := bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(requestAccount.Password))
+	if comparePasswordErr != nil {
+		c.String(http.StatusBadRequest, "login failed2")
+		return
+	}
+
+	session := sessions.Default(c)
+	loginUser, jsonMarshalErr := json.Marshal(account)
+	if jsonMarshalErr == nil {
+		session.Set("loginUser", string(loginUser))
+		session.Save()
+		c.Status(http.StatusOK)
+		return
+	}
+
+	c.Status(http.StatusInternalServerError)
 }
